d5522cd3708b3aa96599274d6a993842538a1d77
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-25 16:31:17 -0500

    Created list of todos

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-  Hello Vue
+  <div class="whole">
+    <div>
+      Chris's List
+    </div>
+    <ol> 
+      <li v-for="todo in todos"> 
+        {{todo.text}}
+      </li>
+    </ol>  
   </div>
 </template>
 
 <script>
-
-
+export default {
+  data() {
+    return {
+      message: 'Hello World', 
+      todos : [
+          {text : 'Eat dinner'},
+          {text : 'Buy books'}, 
+          {text : 'Go to class'}
+      ]
+    }
+  }, 
+};
 </script>
 
 
 <style>
-
+.whole{
+    display: flex;
+    flex-direction: column,;
+    border: 1px solid black;
+    padding-left: 600px;
+}
 </style>

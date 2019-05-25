fadcdf1ddc5bda982d361a4ab7812b45ca410c24
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-25 16:54:53 -0500

    Added vue plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  output: {
+      filename: 'bundle.js', 
+      path: __dirname + '/public'
+  },
+}
